aa32dc49f5a24628813514b53920b507e03cb600
João Pedro <joao.hermenegildocosta@gmail.com>
2026-03-05 20:39:52 -0300

    Alterando o for para find e findIndex nas funções atualizar e remover.

--- CRUD.js
+++ CRUD.js
@@ -93,27 +93,21 @@ function buscar(){
         
             rl.question("\nInsira o código do produto: ", (codigoBusca) => {
         
-            codigoBusca = Number(codigoBusca); /*Feito a conversão de string para número inteiro, pois no rl.question é salvo como string onde iria dar erro na comparação do if.*/
-                let encontrado = false;
+                codigoBusca = Number(codigoBusca); /*Feito a conversão de string para número inteiro, pois no rl.question é salvo como string onde iria dar erro na comparação do if.*/
 
-                    for (let i = 0; i < cadastros.length; i++) {
-                    
-                        if (codigoBusca == cadastros[i].codigo) { 
-                            console.log("\nProduto encontrado com sucesso!");
-                            console.log(`Código: ${cadastros[i].codigo}`);
-                            console.log(`Nome: ${cadastros[i].nome}`);
-                            console.log(`Preço: ${cadastros[i].preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
-                            console.log(`Quantidade: ${cadastros[i].quantidade}\n`);
-                            
-                            encontrado = true;  /* Variável usada para definir que o produto foi encontrado. */
-                            break; /*Break usado para encerrar a busca assim que o produto for encontrado. */
-                        }
-                    }
-
-                    if (!encontrado) { /* Usado ! para verificar se o produto foi encontrado, significa a mesma coisa que "encontrado === false" */
+                const produto = cadastros.find(produto => produto.codigo === codigoBusca);
+                
+                    if (!produto) {
                         console.log("Produto não encontrado!\n\n");
                         mostrarMenu();
+                        return;
                     }
+
+                    console.log("\nProduto encontrado com sucesso!");
+                    console.log(`Código: ${produto.codigo}`);
+                    console.log(`Nome: ${produto.nome}`);
+                    console.log(`Preço: ${produto.preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
+                    console.log(`Quantidade: ${produto.quantidade}\n`);
 
                     mostrarMenu();
             });
@@ -122,26 +116,19 @@ function buscar(){
         
             rl.question("\nInsira o nome do produto: ", (nomeBusca) => {
                 
-                let encontrado = false;
-
-                    for (let i = 0; i < cadastros.length; i++) {
-                    
-                        if (nomeBusca == cadastros[i].nome) { 
-                            console.log("\nProduto encontrado com sucesso!");
-                            console.log(`Código: ${cadastros[i].codigo}`);
-                            console.log(`Nome: ${cadastros[i].nome}`);
-                            console.log(`Preço: ${cadastros[i].preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
-                            console.log(`Quantidade: ${cadastros[i].quantidade}\n`);
-                            
-                            encontrado = true; /* Variável usada para definir que o produto foi encontrado. */
-                            break;
-                        }
-                    }
-                    
-                    if (!encontrado) {  /* Usado ! para verificar se o produto foi encontrado, significa a mesma coisa que "encontrado === false" */
+                const produto = cadastros.find(produto => produto.nome === nomeBusca);
+                
+                    if (!produto) {
                         console.log("Produto não encontrado!\n\n");
                         mostrarMenu();
+                        return;
                     }
+
+                    console.log("\nProduto encontrado com sucesso!");
+                    console.log(`Código: ${produto.codigo}`);
+                    console.log(`Nome: ${produto.nome}`);
+                    console.log(`Preço: ${produto.preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
+                    console.log(`Quantidade: ${produto.quantidade}\n`);
 
                     mostrarMenu();
             });
@@ -165,85 +152,82 @@ function atualizar(){
 
     if (opcaoAtualizar == 1) {
 
-            rl.question(`\nInsira o código do produto que deseja atualizar: `, (codigoAtualizar) =>{
+            rl.question(`\nInsira o código do produto que deseja atualizar: `, (codigoAtualizar) => {
         
-                codigoAtualizar = Number(codigoAtualizar); /*Feito a conversão de string para número inteiro, pois no rl.question é salvo como string onde iria dar erro na comparação do if.*/
-                let encontrado = false;
+            codigoAtualizar = Number(codigoAtualizar); /*Feito a conversão de string para número inteiro, pois no rl.question é salvo como string onde iria dar erro na comparação do if.*/
 
-                    for (let i = 0; i < cadastros.length; i++) {
-            
-                        if (codigoAtualizar == cadastros[i].codigo) {
-                            console.log("\nProduto encontrado com sucesso!");
-                            console.log(`Código: ${cadastros[i].codigo}`);
-                            console.log(`Nome: ${cadastros[i].nome}`);
-                            console.log(`Preço: ${cadastros[i].preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
-                            console.log(`Quantidade atual: ${cadastros[i].quantidade}`);
-                            
-                            encontrado = true;  /* Variável usada para definir que o produto foi encontrado. */
+                /* "i" vai receber o índice do produto encontrado dentro do array "cadastros" */
+                const i = cadastros.findIndex(produto => produto.codigo === codigoAtualizar); /* "produto" usado no findIndex só existe dentro dessa função. */
+                
+                /* findIndex percorre todo o array, ele usa "produto" como parametro para representar cada objeto do array naquele momento, em seguida ele compara "produto.codigo" com "codigoAtualizar".*/
 
-                            rl.question(`\nInsira a nova quantidade: `, (novaQuantidade) => {
-                            
-                            if (novaQuantidade < 0) {
-                                console.log("Quantidade inválida!");
-                                mostrarMenu();
-                                return;
-                            }    
-            
-                            cadastros[i].quantidade = novaQuantidade;
-                            
-                            console.log("Alteração feita com sucesso!");
-                            console.log(`Quantidade atual: ${novaQuantidade}`);
-                            mostrarMenu();
-                            return;
-                            });
-                        }
-                    }      
-
-                    if (!encontrado) {  /* Usado ! para verificar se o produto foi encontrado, significa a mesma coisa que "encontrado === false" */
+                    if (i === -1) { /* Se não for encontrado nenhum código o findIndex retorna -1, nesse caso mostramos a mensagem e voltamos ao menu. */
                         console.log("Produto não encontrado!\n\n");
                         mostrarMenu();
-                    }                
+                        return;
+                    }  
+
+                    const produto = cadastros[i]; /* Produto irá receber as informações de cadastros[i], aonde "i" pode valer (1,2,3 e etc.. dependende da posição do índice na qual foi encontrado), Exemplo : cadastros[2] */
+
+                    console.log("\nProduto encontrado com sucesso!");
+                    console.log(`Código: ${produto.codigo}`); /* produto.codido é a mesma coisa como se fosse cadastros[i].codigo e etc. */
+                    console.log(`Nome: ${produto.nome}`);
+                    console.log(`Preço: ${produto.preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
+                    console.log(`Quantidade: ${produto.quantidade}\n`);
+
+                    rl.question(`Insira a nova quantidade: `, (novaQuantidade) => {
+
+                        if (novaQuantidade < 0) {
+                            console.log("Quantidade inválida!");
+                            mostrarMenu();
+                            return;
+                        }    
+
+                        produto.quantidade = novaQuantidade;
+
+                        console.log("Alteração feita com sucesso!");
+                        console.log(`Quantidade atual: ${novaQuantidade}`);
+                        mostrarMenu();
+                        return;
+                    });
             });
    
         }else if (opcaoAtualizar == 2) {
             
-            rl.question(`\nInsira o nome do produto que deseja atualizar: `, (nomeAtualizar) =>{
-    
-                let encontrado = false;
+            rl.question(`\nInsira o nome do produto que deseja atualizar: `, (nomeAtualizar) => {
 
-                    for (let i = 0; i < cadastros.length; i++) {
-        
-                        if (nomeAtualizar == cadastros[i].nome) {
-                            console.log("\nProduto encontrado com sucesso!");
-                            console.log(`Código: ${cadastros[i].codigo}`);
-                            console.log(`Nome: ${cadastros[i].nome}`);
-                            console.log(`Preço: ${cadastros[i].preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
-                            console.log(`Quantidade atual: ${cadastros[i].quantidade}`);
-
-                            encontrado = true;  /* Variável usada para definir que o produto foi encontrado. */
+                const i = cadastros.findIndex(produto => produto.nome === nomeAtualizar);
             
-                            rl.question(`\nInsira a nova quantidade: `, (novaQuantidade) => {
-                            
-                            if (novaQuantidade < 0) {
-                                console.log("Quantidade inválida!");
-                                mostrarMenu();
-                                return;
-                            }    
-            
-                            cadastros[i].quantidade = novaQuantidade;
-                            
-                            console.log("Alteração feita com sucesso!");
-                            console.log(`Quantidade atual: ${novaQuantidade}`);
-                            mostrarMenu();
-                            return;
-                            });
-                        }
-                    }       
-
-                    if (!encontrado) {  /* Usado ! para verificar se o produto foi encontrado, significa a mesma coisa que "encontrado === false" */
+                    if (i === -1) {
                         console.log("Produto não encontrado!\n\n");
                         mostrarMenu();
+                        return;
                     }
+
+                    const produto = cadastros[i];
+
+                    console.log("\nProduto encontrado com sucesso!");
+                    console.log(`Código: ${produto.codigo}`);
+                    console.log(`Nome: ${produto.nome}`);
+                    console.log(`Preço: ${produto.preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
+                    console.log(`Quantidade: ${produto.quantidade}\n`);
+
+                    rl.question(`Insira a nova quantidade: `, (novaQuantidade) => {
+
+                        if (novaQuantidade < 0) {
+                            console.log("Quantidade inválida!");
+                            mostrarMenu();
+                            return;
+                        }    
+
+                        produto.quantidade = novaQuantidade;
+
+                        console.log("Alteração feita com sucesso!");
+                        console.log(`Quantidade atual: ${novaQuantidade}`);
+                        mostrarMenu();
+                        return;
+
+                    });
             });
 
         }else{
@@ -265,67 +249,59 @@ function remover(){
             
         if (opcaoRemover == 1) {
             
-            rl.question(`\nInsira o código do produto que deseja remover: `, (codigoRemover) => {
-        
-                codigoRemover = Number(codigoRemover); /*Feito a conversão de string para número inteiro, pois no rl.question é salvo como string onde iria dar erro na comparação do if.*/
-                let encontrou = false;
+                rl.question(`\nInsira o código do produto que deseja remover: `, (codigoRemover) => {
+            
+                    codigoRemover = Number(codigoRemover); /*Feito a conversão de string para número inteiro, pois no rl.question é salvo como string onde iria dar erro na comparação do if.*/
+                
+                    const i = cadastros.findIndex(produto => produto.codigo === codigoRemover);
 
-                    for (let i = 0; i < cadastros.length; i++) {
-            
-                        if (codigoRemover == cadastros[i].codigo){
-                            console.log("\nProduto encontrado com sucesso!");
-                            console.log(`Código: ${cadastros[i].codigo}`);
-                            console.log(`Nome: ${cadastros[i].nome}`);
-                            console.log(`Preço: ${cadastros[i].preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
-                            console.log(`Quantidade: ${cadastros[i].quantidade}`);
-
-                            encontrou = true;  /* Variável usada para definir que o produto foi encontrado. */
-            
-                            cadastros.splice(i, 1); /* Utilizado para fazer a remoção do array, remove exatamente 1 elemento na posição i. */
-                            console.log("\nProduto removido com sucesso!\n");
-            
+                        if (i === -1) {
+                            console.log("Produto não encontrado!");
                             mostrarMenu();
-                            return;
+                            return
                         }
-                    }
-
-                    if (!encontrou) {  /* Usado ! para verificar se o produto foi encontrado, significa a mesma coisa que "encontrado === false" */
-                        console.log("Produto não encontrado!\n\n");
-                        mostrarMenu();
-                    }
                         
-            });
+                        const produto = cadastros[i];
+
+                        console.log("\nProduto encontrado com sucesso!");
+                        console.log(`Código: ${produto.codigo}`);
+                        console.log(`Nome: ${produto.nome}`);
+                        console.log(`Preço: ${produto.preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
+                        console.log(`Quantidade: ${produto.quantidade}`);
+
+                        cadastros.splice(i, 1);
+                        console.log("\nProduto removido com sucesso!\n");
+            
+                        mostrarMenu();
+                        return;
+                });
 
         }else if (opcaoRemover == 2) {
 
-            rl.question(`\nInsira o nome do produto que deseja remover: `, (nomeRemover) => {
+                rl.question(`\nInsira o nome do produto que deseja remover: `, (nomeRemover) => {
         
-                let encontrou = false;
+                    const i = cadastros.findIndex(produto => produto.nome === nomeRemover);
 
-                    for (let i = 0; i < cadastros.length; i++) {
-            
-                        if (nomeRemover == cadastros[i].nome){
-                            console.log("\nProduto encontrado com sucesso!");
-                            console.log(`Código: ${cadastros[i].codigo}`);
-                            console.log(`Nome: ${cadastros[i].nome}`);
-                            console.log(`Preço: ${cadastros[i].preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
-                            console.log(`Quantidade: ${cadastros[i].quantidade}`);
-
-                            encontrou = true;  /* Variável usada para definir que o produto foi encontrado. */ 
-            
-                            cadastros.splice(i, 1); /* Utilizado para fazer a remoção do array, remove exatamente 1 elemento na posição i. */
-                            console.log("\nProduto removido com sucesso!\n");
-            
+                        if (i === -1) {
+                            console.log("Produto não encontrado!");
                             mostrarMenu();
-                            return;
+                            return
                         }
-                    }
+                        
+                        const produto = cadastros[i];
 
-                    if (!encontrou) {  /* Usado ! para verificar se o produto foi encontrado, significa a mesma coisa que "encontrado === false" */
-                        console.log("Produto não encontrado!\n\n");
+                        console.log("\nProduto encontrado com sucesso!");
+                        console.log(`Código: ${produto.codigo}`);
+                        console.log(`Nome: ${produto.nome}`);
+                        console.log(`Preço: ${produto.preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})}`);
+                        console.log(`Quantidade: ${produto.quantidade}`);
+
+                        cadastros.splice(i, 1);
+                        console.log("\nProduto removido com sucesso!\n");
+            
                         mostrarMenu();
-                    }              
-            });   
+                        return;       
+                });   
             
         }else{
             console.log("Opção inválida!\n\n");
